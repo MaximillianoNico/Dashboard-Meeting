8712597ecf2340b4c9acf39f53cd8c3857b72270
MaximillianoNico <maximillianonico8@gmail.com>
2019-01-11 15:52:35 +0700

    initialize

---
+++ pages/index/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Index extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+            <div>HomePage</div>
+        )
+    }
+}
+
+export default Index
