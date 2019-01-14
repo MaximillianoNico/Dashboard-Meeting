fab46ef5f73c931ed7f2dfd74331e9bd33afaaf5
MaximillianoNico <maximillianonico8@gmail.com>
2019-01-15 02:11:48 +0700

    change card list

--- pages/index/index.js
+++ pages/index/index.js
@@ -129,6 +129,41 @@ class Index extends React.Component{
                 View
               </button>
             </div>
+            <div className="card-meeting">
+              <div className="title-meeting">
+                <div className="meeting-user-img">
+                  <img className="user-img" src="https://yt3.ggpht.com/a-/AAuE7mD8j66Gv5r4oLf48Ij7PMljbIm_-GrASQ-CSA=s900-mo-c-c0xffffffff-rj-k-no"/>
+                </div>
+                <div className="title-desc">
+                  <div  className="title-desc-name">
+                    <p>Design catchup</p>
+                  </div>
+                  <div className="title-desc-mail">
+                    <p>billgates@microsoft.com</p>
+                  </div>
+                  <p className="title-desc-time">10 : 30 AM</p>
+                </div>
+              </div>
+              <div className="status-meeting">
+                <p className="going">9 Member Going</p>
+                <p className="pending">2 Pending</p>
+              </div>
+              <div className="users-meeting">
+                <div className="users-img">
+                  <img className="img-user1" src="https://yt3.ggpht.com/a-/AAuE7mD8j66Gv5r4oLf48Ij7PMljbIm_-GrASQ-CSA=s900-mo-c-c0xffffffff-rj-k-no" className="user-png"/>
+                  <img src="https://yt3.ggpht.com/a-/AAuE7mD8j66Gv5r4oLf48Ij7PMljbIm_-GrASQ-CSA=s900-mo-c-c0xffffffff-rj-k-no" className="user-png"/>
+                  <img src="https://yt3.ggpht.com/a-/AAuE7mD8j66Gv5r4oLf48Ij7PMljbIm_-GrASQ-CSA=s900-mo-c-c0xffffffff-rj-k-no" className="user-png"/>
+                  <img src="https://yt3.ggpht.com/a-/AAuE7mD8j66Gv5r4oLf48Ij7PMljbIm_-GrASQ-CSA=s900-mo-c-c0xffffffff-rj-k-no" className="user-png"/>
+                  <img src="https://yt3.ggpht.com/a-/AAuE7mD8j66Gv5r4oLf48Ij7PMljbIm_-GrASQ-CSA=s900-mo-c-c0xffffffff-rj-k-no" className="user-png"/>
+                </div>
+                <div className="users-total-count">
+                  <p>+6 more</p>
+                </div>
+              </div>
+              <button className="btn-view">
+                View
+              </button>
+            </div>
           
           </div>
         </div>
